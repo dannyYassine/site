cf6ecb98b3402f9996b8bc68a32e592d942b40fc
Danny Yassine <dannyyassine@gmail.com>
2019-01-24 07:59:54 -0500

    no message

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $directory = '/site';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="<?=$directory?>/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?=$directory?>/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="<?=$directory?>/css/reset.css"/>
     <link rel="stylesheet" href="<?=$directory?>/css/magnific-popup.css"/>
     <link rel="stylesheet" href="<?=$directory?>/css/animate.css"/>
